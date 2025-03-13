0707c6f04baff4b49765c882ea349cc2a02cc75a
pripault <paul.ripault@gmail.com>
2025-03-13 12:30:00 +0100

    init biere-panel Component

---
+++ src/app/biere-panel/biere-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biere-panel',
+  imports: [],
+  templateUrl: './biere-panel.component.html',
+  styleUrl: './biere-panel.component.scss'
+})
+export class BierePanelComponent {
+  
+}
