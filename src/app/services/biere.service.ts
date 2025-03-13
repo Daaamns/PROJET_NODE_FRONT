880cb05ed2e6c36e666046643c8eae4625eccd81
pripault <paul.ripault@gmail.com>
2025-03-13 14:36:09 +0100

    add Biere and Commande types add Biere and Commande panels add Biere-panel methods

---
+++ src/app/services/biere.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Biere } from '../models/types/Biere';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BiereService {
+  private bar_id!: number
+  private http = inject(HttpClient);
+  constructor(bar_id: number) { 
+    this.bar_id = bar_id;
+  }
+  
+  getBieres(): Observable<Biere[]> {
+      return this.http.get<Biere[]>('http://localhost:3000/bars/'+ this.bar_id +'/biere/');
+  }
+  addBiere(biere: Biere): Observable<Biere> {
+    return this.http.post<Biere>('http://localhost:3000/bars/'+ this.bar_id + '/biere/', biere);
+  }
+  updateBiere(biere: Biere): Observable<Biere> {
+    return this.http.put<Biere>('http://localhost:3000/biere/' + biere.id, biere);
+  }
+  deleteBiere(biere: Biere): void {
+    this.http.delete<Biere>('http://localhost:3000/biere/' + biere.id);
+  }
+}
