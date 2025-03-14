fd2a5d19ec8515bfb01ed8c02e07afafe836b4d4
pripault <paul.ripault@gmail.com>
2025-03-14 12:34:14 +0100

    Form addBiere added

--- src/app/biere-page/biere-page.component.ts
+++ src/app/biere-page/biere-page.component.ts
@@ -5,19 +5,38 @@ import { BarService } from '../services/bar.service';
 import { BiereService } from '../services/biere.service';
 import { Biere } from '../models/types/Biere';
 import { NgFor } from '@angular/common';
-import { NgForm, FormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-biere-page',
   standalone: true,
-  imports: [NgFor, FormsModule],
+  imports: [NgFor,ReactiveFormsModule],
   templateUrl: './biere-page.component.html',
   styleUrl: './biere-page.component.scss'
 })
 export class BierePageComponent {
-onSubmit(_t12: any) {
-throw new Error('Method not implemented.');
-}
+  protected readonly biereForm = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl("", Validators.required),
+    description: new FormControl("", Validators.required),
+    degree: new FormControl(),
+    prix: new FormControl(),
+    bar_id: new FormControl(),
+  });
+  onSubmit() {
+    console.log(this.biereForm.valid);
+    console.log(this.biereForm.value.name)
+    console.log(this.biereForm.value.description)
+    console.log(this.biereForm.value.degree)
+    console.log(this.biereForm.value.prix)
+    if (this.biereForm.valid){
+      let beer : Biere = this.biereForm.value as Biere
+      this.biereService.addBiere(this.id, beer).subscribe((data) =>{
+        console.log(data);
+        
+      })
+    }
+  }
   id!: number;
   bar!: Bar;
   private biereService: BiereService = new BiereService()
