c4601ac3697e0506cff02ffbe95767386a663011
pripault <paul.ripault@gmail.com>
2025-03-17 11:20:59 +0100

    Add post form and delete option

--- src/app/biere-page/biere-page.component.ts
+++ src/app/biere-page/biere-page.component.ts
@@ -4,17 +4,29 @@ import { ActivatedRoute } from '@angular/router';
 import { BarService } from '../services/bar.service';
 import { BiereService } from '../services/biere.service';
 import { Biere } from '../models/types/Biere';
-import { NgFor } from '@angular/common';
+import { CurrencyPipe, NgFor, NgIf } from '@angular/common';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-biere-page',
   standalone: true,
-  imports: [NgFor,ReactiveFormsModule],
+  imports: [NgFor,ReactiveFormsModule, NgIf, TableModule, CurrencyPipe, ButtonModule, DialogModule],
   templateUrl: './biere-page.component.html',
   styleUrl: './biere-page.component.scss'
 })
 export class BierePageComponent {
+
+  biereUpdate = new FormGroup({
+    id: new FormControl(),
+    name: new FormControl("", Validators.required),
+    description: new FormControl("", Validators.required),
+    degree: new FormControl(),
+    prix: new FormControl(),
+    bar_id: new FormControl(),
+  });
   protected readonly biereForm = new FormGroup({
     id: new FormControl(),
     name: new FormControl("", Validators.required),
@@ -23,40 +35,67 @@ export class BierePageComponent {
     prix: new FormControl(),
     bar_id: new FormControl(),
   });
-  onSubmit() {
-    console.log(this.biereForm.valid);
-    console.log(this.biereForm.value.name)
-    console.log(this.biereForm.value.description)
-    console.log(this.biereForm.value.degree)
-    console.log(this.biereForm.value.prix)
-    if (this.biereForm.valid){
-      let beer : Biere = this.biereForm.value as Biere
-      this.biereService.addBiere(this.id, beer).subscribe((data) =>{
-        console.log(data);
-        
-      })
-    }
-  }
+  visibleDelete!: boolean;
+  
   id!: number;
   bar!: Bar;
   private biereService: BiereService = new BiereService()
   beers: Biere[] = []
+  visibleEdit!: boolean;
+  selectedBiere!: Biere;
 
   constructor(private route: ActivatedRoute, private barService: BarService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
       this.id = params['id'];
-      console.log('ID récupéré :', this.id);
     });
-
+    this.loadData();
+  }
+  loadData(): void {
     this.barService.getBarById(this.id).subscribe((data) => {
       this.bar = data;
-      console.log(this.bar);
     });
     this.biereService.getBieres(this.id).subscribe((data) =>{
       this.beers = data;
-      console.log(data);
+    });
+  }
+  onUpdateSubmit() {
+    if (this.biereUpdate.valid){
+      let beer : Biere = this.biereUpdate.value as Biere
+      beer.id = this.selectedBiere.id;
+      this.biereService.updateBiere(beer).subscribe((data) =>{
+        console.log(data);
+        
+      })
+    }
+    console.log(this.biereUpdate.value);
+    
+    this.loadData()
+    }
+  onAddSubmit() {
+    if (this.biereForm.valid){
+      let beer : Biere = this.biereForm.value as Biere
+      this.biereService.addBiere(this.id, beer).subscribe((data) =>{
+      })
+    }
+    this.loadData()
+  }
+  openEditDialog(biere: Biere) {
+      this.selectedBiere = { ...biere };
+      console.log(this.selectedBiere);
+      
+      this.visibleEdit = true;
+  }
+  openDeleteDialog(biere: Biere) {
+    this.selectedBiere = biere;
+    this.visibleDelete = true;
+  
+  }
+  deleteCommande() {
+    this.biereService.deleteBiere(this.selectedBiere).subscribe(() => {
+      this.visibleDelete = false;
+      this.loadData();
     });
   }
 
